491446f14f97578cb88f7d0068ef4cb16dc1cd58
Joy1618 <43145075+Joy1618@users.noreply.github.com>
2020-09-16 07:07:41 -0700

    Update test.java

--- test.java
+++ test.java
@@ -1,1 +1,24 @@
 
+import static org.junit.Assert.assertArrayEquals;
+import org.junit.jupiter.api.Test;
+
+public class test extends Ex{
+	
+	//unsorted array of integers
+			int[] unsorted = new int[]{2,1,4,3};
+			//sorted array
+			int[] sorted = sort(unsorted);
+	    //descending sorted array
+	    int[] DescSorted = new int[]{4,3,2,1};
+	  
+	  @Test
+	    public void InsertionSortTest() {
+	        int[] t = InsertionSort(unsorted);
+			    assertArrayEquals(sorted,t);
+	  }
+	   @Test
+	    public void DescTest() {
+	        int[] s = reverse(sorted);
+			    assertArrayEquals(DescSorted,s);
+	}
+}

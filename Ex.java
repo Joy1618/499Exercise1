22db2c853461d6a3f3cf1762f8cfa2024592b231
Joy1618 <43145075+Joy1618@users.noreply.github.com>
2020-09-16 05:52:06 -0700

    Insertion-Sort
    Adding the method of sorting via insertion sort and printing the result to see if it is the same as default sorting

--- Ex.java
+++ Ex.java
@@ -13,9 +13,24 @@ public class sorttest extends Ex{
   int[] sorted = sort(unsorted);
   }
 
+public int[] InsertionSort(int[] arr){
+	for(int i = 1; i < arr.length; i++) {
+		int k = arr[i];
+		int pos = i;
+		while(pos > 0 && k < arr[pos-1]) {
+			arr[pos] = arr[pos-1];
+			pos--;
+		}
+		arr[pos] = k;
+		}
+	return arr;
+	}
+
 public static void main(String[] args) {	
 	int[] unsorted = new int[] {2,1,4,3};
 	int[] sorted = sort(unsorted);
+	int[] sorted2 = InsertionSort(unsorted);
 	System.out.println(Arrays.toString(sorted));
+	System.out.println(Arrays.toString(sorted2));
 	}
 }
